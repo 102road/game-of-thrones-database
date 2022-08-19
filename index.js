e01251e2a5cba5bb48cc06883f44d3c36e36975c
102road <bill.brown094@gmail.com>
2022-08-19 18:38:52 +0100

    expand database

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bp = require("body-parser");
-
 const fs = require("fs");
+
+app.use(cors());
+app.use(express.json());
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
 app.get("/characters", (res, req) => {
   res.json(fs.readFileSync("./data.json"));
