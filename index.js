1d7ae6d585434d4cae87348fc6b486ab432fcc82
102road <bill.brown094@gmail.com>
2022-08-21 18:00:36 +0100

    minor changes

--- index.js
+++ index.js
@@ -9,16 +9,34 @@ app.use(express.json());
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
-app.get("/characters", (req, res) => {
+app.get("/", (req, res) => {
   const result = JSON.parse(fs.readFileSync("./data.json"));
   res.send(result);
 });
 
-app.post("/characters", (req, res) => {
+app.get("/results", (req, res) => {
+  const result = JSON.parse(fs.readFileSync("./results.json"));
+  res.send(result);
+});
+
+app.post("/", (req, res) => {
   const character = req.headers.id;
   const data = JSON.parse(fs.readFileSync("./results.json"));
   data.character = data.character + 1;
   fs.writeFileSync("./results.json", data);
+});
+
+app.post("/hi", (req, res) => {
+  const result = JSON.parse(fs.readFileSync("./data.json"));
+  let object = {};
+  result.forEach((element) => {
+    object.element.fullName = 0;
+  });
+  fs.writeFileSync("./results.json", object, "utf-8", (err) => {
+    if (err) {
+      console.log("There was an error when attempting to add new user", err);
+    }
+  });
 });
 
 app.listen(4000, () => {
